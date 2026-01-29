33db01e44d37c70e8a90588397b42c148c08e882
Rahul Ukey <141477302+rahuluke@users.noreply.github.com>
2026-01-30 00:04:56 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,47 +2,44 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-
-app.use(
-  cors({
-    origin: "https://lms-o0og.onrender.com",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.options("*", cors());
-
-// middlewares
+// default middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "https://lms-nswg.onrender.com",
+    credentials: true,
+  }),
+);
 
-// routes
+const _dirname = path.resolve();
+
+// apis
 app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 
-// frontend
-const _dirname = path.resolve();
-app.use(express.static(path.join(_dirname, "Client", "dist")));
-app.get("*", (_, res) => {
+app.use(express.static(path.join(_dirname, "/Client/dist")));
+app.get(/.*/, (_, res) => {
   res.sendFile(path.resolve(_dirname, "Client", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listen at port ${PORT}`);
 });
